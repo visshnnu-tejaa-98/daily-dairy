987cc23e549f34826a71f66af2c966efe2b25bfd
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-04-02 15:49:48 +0530

    reset route implmented

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post('/register', async (req, res) => {
 				to: req.body.email,
 				subject: 'Registration Successful!!!',
 				html: `
-               <p>Hi { req.body.name}</p>
+               <p>Hi ${req.body.name}</p>
                <p>Thank you for Registering <strong>Daily Dairy</strong></p>
                <p>Login to explore more</p>
                <p>${process.env.FRONTEND_URL}/login</p>
@@ -116,6 +116,28 @@ app.post('/forgot', async (req, res) => {
 				.json({ message: 'Reset mail sent to specified email, please check your email' });
 		} else {
 			res.status(400).json({ message: "Email Doestn't exist, Try Again with valid Email" });
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({ message: 'something went wrong' });
+	}
+});
+
+app.post('/reset', async (req, res) => {
+	try {
+		const client = await mongoClient.connect(DB_URL);
+		const db = client.db(DATA_BASE);
+		const user = await db.collection(USERS_COLLECTION).findOne({ email: req.body.email });
+		if (user) {
+			const salt = await bcrypt.genSalt(10);
+			const hash = await bcrypt.hash(req.body.password, salt);
+			req.body.password = hash;
+			await db
+				.collection(USERS_COLLECTION)
+				.updateOne({ email: req.body.email }, { $set: { password: req.body.password } });
+			res.status(200).json({ message: 'Password reseted successfully' });
+		} else {
+			res.status(400).json({ message: "User Doesn't exists, Try again with valid email" });
 		}
 	} catch (error) {
 		console.log(error);
